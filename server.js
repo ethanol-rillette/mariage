7bb95a961cc73499444aee10b983b4e16aa614b6
ethanol-rillette <ethan.mezerette@orange.fr>
2026-06-12 17:57:49 +0200

    transporter

--- server.js
+++ server.js
@@ -75,17 +75,17 @@ app.post('/upload', async (req, res) => {
         try {
           const extension = type === 'photo' ? 'png' : 'webm';
 
-          await transporter.sendMail({
-            from: process.env.EMAIL_USER,
-            to: process.env.EMAIL_TO,
-            subject: `Nouvelle ${type} 📸`,
-            text: `Upload par ${uploader || 'invité'}`,
-            attachments: [
-              {
-                filename: `${type}_${Date.now()}.${extension}`,
-                content: buffer
-              }
-            ]
+          const dns = require('dns');
+          dns.setDefaultResultOrder('ipv4first');
+
+          const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false,
+            auth: {
+              user: process.env.EMAIL_USER,
+              pass: process.env.EMAIL_PASS
+            }
           });
 
           console.log("✔ Email envoyé");
